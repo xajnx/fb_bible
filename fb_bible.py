3503bb067df416d66b76d74efa4408570fa9f97c
xajnx <aaron.nelson805@gmail.com>
2017-06-23 08:19:03 -0500

    added logging system.    previous version would start from beginning(Gen1:1) if script died or computer was restarted. this patch creates a log of last verse posted that can be called when the script is restarted

--- fb_bible.py
+++ fb_bible.py
@@ -7,7 +7,9 @@ import facebook
 
 #set variables
 home = os.environ['HOME']
-work = home + '/scripts/python/fb_bible/KJVJson/'
+work = home + '/scripts/python/fb_bible/'
+bible_dir = work + 'KJVJson/'
+bible_log = work + 'last.log'
 
 with open(home + '/scripts/python/apikeys', 'r') as f:
     keys = eval(f.read())
@@ -27,28 +29,55 @@ bible = ['Genesis', 'Exodus', 'Lev', 'Num', 'Deut', 'Joshua', 'Judges',
          'Romans', '1Cor', '2Cor', 'Gal', 'Eph', 'Philip', 'Hebrews','James',
          '1Peter', '2Peter', '1John', '2John', '3John', 'Jude', 'Rev']
 
-verses = []
-def verse_an_hour():
-    for book in bible:
-        get = work + book + '.json'
+def fb_bible():
+    if os.path.exists(bible_log) == False:
+        for book in bible:
+            get = bible_dir + book + '.json'
+            with open(get, 'r') as f:
+                data = json.load(f)
+
+            num_verse = range(len(data['verses'])-1)
+            for n in num_verse:
+                verse = data['verses'][n]['verse']
+                chap = data['verses'][n]['chapter']
+                text = data['verses'][n]['text']
+                verse_str = text + ' - ' + book + str(chap) + ':' + str(verse)
+                log_str = {'book': book, 'start': n}
+                graph.put_object(parent_object='me', connection_name='feed', message=verse_str)
+
+                with open(bible_log, 'w+') as log:
+                    log.write(json.dumps(log_str))
+                pause.hours(1)
+
+    elif os.path.exists(bible_log) == True:
+        with open(bible_log, 'r') as log:
+            data = eval(log.read())
+        book = data['book']
+        start = data['start']
+        get = bible_dir + book + '.json'
         with open(get, 'r') as f:
             data = json.load(f)
 
-        num_verse = range(len(data['verses'])-1)
-        for n in num_verse:
+        num_verse = len(data['verses']) - 1
+        for n in range(start, num_verse):
             verse = data['verses'][n]['verse']
             chap = data['verses'][n]['chapter']
             text = data['verses'][n]['text']
             verse_str = text + ' - ' + book + str(chap) + ':' + str(verse)
-            verses.append(verse_str)
+            log_str = {'book':book, 'start': n}
+            graph.put_object(parent_object='me', connection_name='feed', message=verse_str)
 
-    for verse in verses:
-        graph.put_object(parent_object='me', connection_name='feed', message=verse)
-        pause.hours(1) #change this as needed
+            with open(bible_log, 'w+') as log:
+                log.write(json.dumps(log_str))
+            pause.hours(1)
+
 
 if __name__ == '__main__':
 
-    verse_an_hour()
+    fb_bible()
+
+
+
 
 
 
